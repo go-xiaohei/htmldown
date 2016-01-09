0e0656479161cfbe435e7b0f39f02919333c3e7f
fuxiaohei <fuxiaohei@vip.qq.com>
2016-01-09 16:29:39 +0800

    refactor to Node and Documet struct

---
+++ document.go
@@ -0,0 +1,115 @@
+package ng
+
+import (
+	"io"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type (
+	// Document maintains nodes of html content
+	Document struct {
+		htmlReader io.Reader
+
+		nodes    map[string]Node
+		rootNode Node
+		isParsed bool
+	}
+)
+
+// NewDocument returns new document with io.Reader
+func NewDocument(r io.Reader) *Document {
+	return &Document{
+		htmlReader: r,
+		nodes: map[string]Node{
+			"p":          new(ParagraphNode),
+			"a":          new(HrefNode),
+			"img":        new(ImageNode),
+			"hr":         new(HorizontalNode),
+			"br":         new(BreakNode),
+			"blockquote": new(BlockquoteNode),
+			"code":       new(CodeNode),
+		},
+		rootNode: &RootNode{},
+	}
+}
+
+func (doc *Document) parse() error {
+	if doc.isParsed {
+		return nil
+	}
+	var (
+		z       = html.NewTokenizer(doc.htmlReader)
+		current = doc.rootNode
+		err     error
+	)
+
+	for {
+		next := z.Next()
+		if next == html.ErrorToken {
+			if z.Err() != io.EOF {
+				err = z.Err()
+			}
+			break
+		}
+		t := z.Token()
+
+		// start tag, self close tag
+		if next == html.StartTagToken || next == html.SelfClosingTagToken {
+			tag := strings.ToLower(t.Data)
+			n := new(CommonNode).New(t, current)
+			if n2, ok := doc.nodes[tag]; ok {
+				n = n2.New(t, current)
+			}
+			if next == html.StartTagToken {
+				current = n
+			} else {
+				current.Children(n)
+			}
+			continue
+		}
+
+		// text tag
+		if next == html.TextToken {
+			if len(t.Data) > 0 && t.Data != "\n" {
+				current.Text(t.Data)
+			}
+			continue
+		}
+
+		// end tag
+		if next == html.EndTagToken {
+			current.Parent().Children(current)
+			current = current.Parent()
+		}
+	}
+
+	if current != nil && current.Parent() != nil {
+		current.Parent().Children(current)
+	}
+
+	doc.isParsed = true
+	return err
+}
+
+// Root returns root node of the document
+func (doc *Document) Root() Node {
+	return doc.rootNode
+}
+
+// SetNode sets node by tag name.
+// so it parses element by this node
+func (doc *Document) SetNode(tag string, n Node) {
+	doc.nodes[tag] = n
+}
+
+// Markdown renders this document to markdown content
+func (doc *Document) Markdown() (string, error) {
+	if !doc.isParsed {
+		if err := doc.parse(); err != nil {
+			return "", err
+		}
+	}
+	return doc.rootNode.Markdown(), nil
+}
--- node.go
+++ node.go
@@ -1,75 +1,210 @@
-package htmldown
+package ng
 
 import (
 	"fmt"
 	"strings"
+
+	"golang.org/x/net/html"
 )
 
-type Node struct {
-	Tag        string
-	Text       string
-	Attributes map[string]string
-	Children   []*Node
-	Parent     *Node
+type (
+	// Node defines a simple description iof html node
+	Node interface {
+		New(t html.Token, parent Node) Node
+		Tag() string                   // node's tag name
+		Text(str ...string) string     // get or append node's text
+		Attr() map[string]string       // node's attributes
+		Parent() Node                  // node's parent
+		Children(nodes ...Node) []Node // append or get node's children nodes
+		Markdown() string              // render nodes to markdown
+	}
+	baseNode struct {
+		tag        string
+		text       string
+		attributes map[string]string
+		parent     Node
+		children   []Node
+	}
+	// RootNode is root node of html document
+	RootNode struct {
+		baseNode
+	}
+	// CommonNode is a node that print as normal html, not markdown
+	CommonNode struct {
+		baseNode
+	}
+)
 
-	IsEnd bool
+// New returns new baseNode
+func (bn *baseNode) New(t html.Token, parent Node) Node {
+	b := &baseNode{
+		tag:        strings.ToLower(t.Data),
+		attributes: make(map[string]string),
+		parent:     parent,
+	}
+	for _, attr := range t.Attr {
+		b.attributes[attr.Key] = attr.Val
+	}
+	return b
 }
 
-func NewNode(tag string) *Node {
-	return &Node{
-		Tag:        tag,
-		Attributes: make(map[string]string),
+func (bn *baseNode) Tag() string { return bn.tag }
+
+func (bn *baseNode) Attr() map[string]string { return bn.attributes }
+
+func (bn *baseNode) Text(str ...string) string {
+	if len(str) > 0 {
+		bn.text += strings.Join(str, "")
+	}
+	return bn.text
+}
+
+func (bn *baseNode) Parent() Node { return bn.parent }
+
+func (bn *baseNode) Children(nodes ...Node) []Node {
+	if len(nodes) > 0 {
+		bn.children = append(bn.children, nodes...)
+		bn.Text(strings.Repeat("@node@", len(nodes)))
+	}
+	return bn.children
+}
+
+func (bn *baseNode) Markdown() string {
+	content := bn.text
+	for _, child := range bn.children {
+		content = strings.Replace(content, "@node@", child.Markdown(), 1)
+	}
+	return content
+}
+
+// Tag returns RootNode's tag ,"root"
+func (rt *RootNode) Tag() string { return "root" }
+
+// Markdown renders CommonNode as html tag string ,<tag>text</tag>
+func (cn *CommonNode) Markdown() string {
+	return fmt.Sprintf("<%s>%s</%s>", cn.Tag(), cn.baseNode.Markdown(), cn.Tag())
+}
+
+// New returns new CommonNode
+func (cn *CommonNode) New(t html.Token, parent Node) Node {
+	bs := cn.baseNode.New(t, parent)
+	return &CommonNode{
+		baseNode: *(bs.(*baseNode)),
 	}
 }
 
-func (node *Node) AddText(text string) {
-	node.Text += text
+type (
+	// ParagraphNode is used to <p>
+	ParagraphNode struct{ baseNode }
+	// HrefNode is used to <a>
+	HrefNode struct{ baseNode }
+	// ImageNode is used to <img>
+	ImageNode struct{ baseNode }
+	// HorizontalNode is used to <hr/>
+	HorizontalNode struct{ baseNode }
+	// BreakNode is used to <br/>
+	BreakNode struct{ baseNode }
+	// BlockquoteNode is used to <blockquote>
+	BlockquoteNode struct{ baseNode }
+	// CodeNode is used to <code>
+	CodeNode struct{ baseNode }
+)
+
+// Markdown renders ParagraphNode with \n
+func (pn *ParagraphNode) Markdown() string {
+	if pn.Text() == "" {
+		return ""
+	}
+	return pn.baseNode.Markdown() + "\n\n"
 }
 
-func (node *Node) AddNode(n *Node) {
-	node.Children = append(node.Children, n)
-	node.Text += fmt.Sprintf("@node%d@", len(node.Children)-1)
-	n.IsEnd = true
+// New returns new ParagraphNode
+func (pn *ParagraphNode) New(t html.Token, parent Node) Node {
+	bs := pn.baseNode.New(t, parent)
+	return &ParagraphNode{
+		baseNode: *(bs.(*baseNode)),
+	}
 }
 
-func (node *Node) Markdown() string {
-	for i, child := range node.Children {
-		node.Text = strings.Replace(
-			node.Text,
-			fmt.Sprintf("@node%d@", i),
-			child.Markdown(),
-			-1,
-		)
+// Markdown renders HrefNode as [text](href)
+func (hn *HrefNode) Markdown() string {
+	return fmt.Sprintf("[%s](%s)", hn.baseNode.Markdown(), hn.attributes["href"])
+}
+
+// New returns new HrefNode
+func (hn *HrefNode) New(t html.Token, parent Node) Node {
+	bs := hn.baseNode.New(t, parent)
+	return &HrefNode{
+		baseNode: *(bs.(*baseNode)),
 	}
-	if node.Tag == "code" && node.Attributes["class"] != "" {
-		return fmt.Sprintf(`<%s class="%s">%s</%s>`, node.Tag, node.Attributes["class"], node.Text, node.Tag)
+}
+
+// Markdown renders ImageNode as ![alt](src)
+func (in *ImageNode) Markdown() string {
+	return fmt.Sprintf("![%s](%s)", in.attributes["alt"], in.attributes["src"])
+}
+
+// New returns new ImageNode
+func (in *ImageNode) New(t html.Token, parent Node) Node {
+	bs := in.baseNode.New(t, parent)
+	return &ImageNode{
+		baseNode: *(bs.(*baseNode)),
 	}
-	switch node.Tag {
-	case "a":
-		return fmt.Sprintf("[%s](%s)", node.Text, node.Attributes["href"])
-	case "img":
-		return fmt.Sprintf("\n![%s](%s)\n", node.Attributes["alt"], node.Attributes["src"])
-	case "p", "br":
-		return node.Text + "\n"
-	case "strong", "b":
-		return fmt.Sprintf("**%s**", node.Text)
-	case "em":
-		return fmt.Sprintf("**%s**", node.Text)
-	case "hr":
-		return "\n---\n"
-	case "blockquote":
-		return fmt.Sprintf("\n```\n%s\n```\n", node.Text)
-	case "h1", "h2", "h3", "h4", "h5", "h6":
-		repeat := int(node.Tag[len(node.Tag)-1]) - 48
-		return fmt.Sprintf("%s %s", strings.Repeat("#", repeat), node.Text)
-	default:
-		if node.Tag != "" && node.Tag != "root" {
-			return fmt.Sprintf("<%s>%s</%s>", node.Tag, node.Text, node.Tag)
-		}
-		if node.Text == "\n" {
-			return ""
-		}
-		return node.Text
+}
+
+// Markdown renders HorizontalNode as `---\n`
+func (hn *HorizontalNode) Markdown() string {
+	return "---\n\n"
+}
+
+// New returns HorizontalNode
+func (hn *HorizontalNode) New(t html.Token, parent Node) Node {
+	bs := hn.baseNode.New(t, parent)
+	return &HorizontalNode{
+		baseNode: *(bs.(*baseNode)),
 	}
-	return node.Text
+}
+
+// Markdown renders BreakNode as "\n\n"
+func (bn *BreakNode) Markdown() string {
+	return "\n\n"
+}
+
+// New returns new BreakNode
+func (bn *BreakNode) New(t html.Token, parent Node) Node {
+	bs := bn.baseNode.New(t, parent)
+	return &BreakNode{
+		baseNode: *(bs.(*baseNode)),
+	}
+}
+
+// Markdown renders BlockquoteNode as  \n```\n text \n```\n
+func (bn *BlockquoteNode) Markdown() string {
+	return fmt.Sprintf("\n```\n%s\n```\n", bn.baseNode.Markdown())
+}
+
+// New returns new BlockquoteNode
+func (bn *BlockquoteNode) New(t html.Token, parent Node) Node {
+	bs := bn.baseNode.New(t, parent)
+	return &BlockquoteNode{
+		baseNode: *(bs.(*baseNode)),
+	}
+}
+
+// Markdown renders CodeNode as <code>text</code>
+// if element class exist, render as <code class="class">text</code>
+func (cn *CodeNode) Markdown() string {
+	class := cn.attributes["class"]
+	if class != "" {
+		return fmt.Sprintf(`<code class="%s">%s</code>`, class, cn.baseNode.Markdown())
+	}
+	return fmt.Sprintf("<code>%s</code>", cn.baseNode.Markdown())
+}
+
+// New returns new CodeNode
+func (cn *CodeNode) New(t html.Token, parent Node) Node {
+	bs := cn.baseNode.New(t, parent)
+	return &CodeNode{
+		baseNode: *(bs.(*baseNode)),
+	}
 }
